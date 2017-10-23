059d9ac5274a74f6cff5e5bc5bc29dc0b0881e81
Christian Giménez <christian.gimenez@sigis.com.ve>
2017-10-23 12:53:17 -0400

    Se agrego lo siguiente:
    - Session sencilla.
    - Main: componente principal.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {reactLocalStorage} from 'reactjs-localstorage';
+import {BrowserRouter as Router, Route, Link, Redirect, withRouter} from 'react-router-dom';
 import {Login} from './components/Login';
+import {Main} from './components/Main';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-		  <Login />
-      </div>
-    );
-  }
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            layout: ''
+        };
+    }
+    
+    componentDidMount() {
+        let savedSession = (reactLocalStorage.get('session') !== undefined) ? reactLocalStorage.get('session') : '';
+        if (savedSession !== '') {
+            this.setState({layout : 'session-active'});
+        } else {
+            this.setState({layout : 'session-inactive'});
+        }
+    }
+    
+    render() {
+        if (this.state.layout === 'session-active') {
+            return (
+            <div className="App">
+                <Main />
+            </div>
+            );
+        } else if (this.state.layout === 'session-inactive') {
+            return (
+            <div className="App">
+                <Login />
+            </div>
+            );
+        } else {
+            return false;
+        }
+    }
+
 }
 
 export default App;
